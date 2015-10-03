adacd8249a09de823d970e5e7fab06b9c5174bfd
phuchit <ripzery@gmail.com>
2015-10-03 20:08:09 +0700

    Add map to emergency fragment

--- app/src/main/java/com/socket9/tsl/Fragments/EmergencyFragment.java
+++ app/src/main/java/com/socket9/tsl/Fragments/EmergencyFragment.java
@@ -1,19 +1,34 @@
 package com.socket9.tsl.Fragments;
 
 
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.google.android.gms.maps.MapView;
 import com.socket9.tsl.R;
+import com.socket9.tsl.Utils.MapHelper;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import timber.log.Timber;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class EmergencyFragment extends Fragment {
 
+
+    @Bind(R.id.mapView)
+    MapView mapView;
+    @Bind(R.id.btnRequest)
+    Button btnRequest;
+    private MapHelper mapHelper;
+    private MapHelper.MapListener mapListener;
 
     public EmergencyFragment() {
         // Required empty public constructor
@@ -25,8 +40,50 @@ public class EmergencyFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_emergency, container, false);
+        ButterKnife.bind(this, rootView);
+        mapHelper = new MapHelper();
+        mapHelper.initMap(getContext(), mapView, savedInstanceState);
+        mapHelper.getMap().setMyLocationEnabled(true);
+        mapListener = new MapHelper.MapListener() {
+            @Override
+            public void onMyLocationChanged(Location location) {
+                Timber.i("%s",location.toString());
+            }
+        };
         return rootView;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapHelper.getmMapView().onResume();
+        mapHelper.setOnMyLocationChangeListener(mapListener);
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapHelper.getmMapView().onPause();
+        mapHelper.removeOnMyLocationChangeListener();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapHelper.getmMapView().onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapHelper.getmMapView().onLowMemory();
+    }
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
---
+++ app/src/main/java/com/socket9/tsl/Utils/MapHelper.java
@@ -0,0 +1,173 @@
+package com.socket9.tsl.Utils;
+
+import android.content.Context;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.socket9.tsl.R;
+
+/**
+ * Created by visit on 9/16/15 AD.
+ */
+public class MapHelper {
+    //    This latlng is the center location of Samui island
+    public static double latitude = 9.498625;
+    public static double longitude = 99.994406;
+    private GoogleMap mMap;
+    private MapView mMapView;
+    private MapListener mapListener = null;
+
+    public void initMap(Context context, View rootView, Bundle savedInstanceState) {
+        mMapView = (MapView) rootView.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();
+        try {
+            MapsInitializer.initialize(context);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mMap = mMapView.getMap();
+    }
+
+    public void initMap(Context context, MapView mMapView, Bundle savedInstanceState) {
+        this.mMapView = mMapView;
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();
+        try {
+            MapsInitializer.initialize(context);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mMap = mMapView.getMap();
+    }
+
+    public GoogleMap getMap() {
+        return mMap;
+    }
+
+    public MapView getmMapView() {
+        return mMapView;
+    }
+
+    public Marker addMarker(double latitude, double longitude, int markerIcon) {
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title("Hello Maps");
+
+
+        if (markerIcon != 0) {
+            marker.icon(BitmapDescriptorFactory.fromResource(markerIcon));
+        }
+
+        try {
+            return mMap.addMarker(marker);
+        } catch (NullPointerException e) {
+            throw new NullPointerException("Please call initMap method first!");
+        }
+    }
+
+    public Marker addMarkerThenZoom(double latitude, double longitude, int zoomLevel, int markerIcon) {
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title("Hello Maps");
+
+        if (markerIcon == 0) {
+            // Changing marker icon
+//            marker.icon(BitmapDescriptorFactory
+//                    .defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+        } else {
+            marker.icon(BitmapDescriptorFactory.fromResource(markerIcon));
+        }
+
+        // adding marker
+        Marker mMarker = mMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude, longitude)).zoom(zoomLevel).build();
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        try {
+            return mMarker;
+        } catch (NullPointerException e) {
+            throw new NullPointerException("Please call initMap method first!");
+        }
+    }
+
+    public void moveTo(double latitude, double longitude, int zoomLevel) {
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude, longitude)).zoom(zoomLevel).build();
+        mMap.moveCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+    }
+
+    public void zoomTo(double latitude, double longitude, int zoomLevel) {
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latitude, longitude)).zoom(zoomLevel).build();
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+    }
+
+    public void zoomTo(Marker marker, int zoomLevel) {
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(marker.getPosition().latitude, marker.getPosition().longitude)).zoom(zoomLevel).build();
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+    }
+
+    public void setOnMyLocationChangeListener(MapListener mapListener) {
+        try {
+            this.mapListener = mapListener;
+            mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+                @Override
+                public void onMyLocationChange(Location location) {
+                    MapHelper.this.mapListener.onMyLocationChanged(location);
+                }
+            });
+        } catch (NullPointerException e) {
+            throw new NullPointerException("You must not be set the parameter to null!");
+        }
+    }
+
+    public void removeOnMyLocationChangeListener() {
+        mapListener = null;
+        mMap.setOnMyLocationChangeListener(null);
+    }
+
+    public Marker addMarkerThenZoom(LatLng latlng, int zoomLevel) {
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latlng.latitude, latlng.longitude)).title("Hello Maps");
+
+        // Changing marker icon
+//        marker.icon(BitmapDescriptorFactory
+//                .defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+        // adding marker
+        Marker mMarker = mMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(latlng.latitude, latlng.longitude)).zoom(zoomLevel).build();
+        mMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        try {
+            return mMarker;
+        } catch (NullPointerException e) {
+            throw new NullPointerException("Please call initMap method first!");
+        }
+
+    }
+
+    public interface MapListener {
+        void onMyLocationChanged(Location location);
+    }
+}
