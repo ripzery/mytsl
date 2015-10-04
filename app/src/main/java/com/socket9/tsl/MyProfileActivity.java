9069efa1d444f1aaafaa763735e6cf0eb8971d68
phuchit <ripzery@gmail.com>
2015-10-04 18:49:55 +0700

    able to take a photo

--- app/src/main/java/com/socket9/tsl/MyProfileActivity.java
+++ app/src/main/java/com/socket9/tsl/MyProfileActivity.java
@@ -1,8 +1,12 @@
 package com.socket9.tsl;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -28,13 +32,24 @@ public class MyProfileActivity extends BaseActivity {
     EditText etPassword;
     @Bind(R.id.etAddress)
     EditText etAddress;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
         ButterKnife.bind(this);
+        setListener();
         initToolbar(myToolbar, "My Profile", true);
+    }
+
+    public void setListener(){
+        ivUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
 
     @Override
@@ -42,6 +57,22 @@ public class MyProfileActivity extends BaseActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_my_profile, menu);
         return true;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            ivUser.setImageBitmap(imageBitmap);
+        }
     }
 
 }
